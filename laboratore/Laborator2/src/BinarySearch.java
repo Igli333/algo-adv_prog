b230f307a0da00bcf80051d013d95f22547a10cb
Igli Balla <igliballa333@outlook.com>
2023-01-11 22:59:40 +0100

    Final edits

--- laboratore/Laborator2/src/BinarySearch.java
+++ laboratore/Laborator2/src/BinarySearch.java
@@ -11,7 +11,7 @@ public class BinarySearch {
         int middle = (end + start) / 2;
 
         if (array[middle] == element) {
-            if(((middle - 1) > -1 && array[middle - 1] != element) || middle == 0){
+            if (middle == 0 || (array[middle - 1] != element)) {
                 return middle;
             }
         } else if (array[middle] < element) {
@@ -32,13 +32,31 @@ public class BinarySearch {
         int middle = (end + start) / 2;
 
         if (array[middle] == element) {
-            if(((middle + 1) < array.length && array[middle + 1] != element) || middle == array.length - 1){
+            if ((middle == array.length - 1) || (array[middle + 1] != element)) {
                 return middle;
             }
         } else if (array[middle] > element) {
             return lastIndexBinarySearch(array, element, 0, middle - 1);
         }
         return lastIndexBinarySearch(array, element, middle + 1, end);
+    }
+
+    public int findMostRepeatedValue(int[] array) {
+        int[] indexes = new int[array[array.length - 1] + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            indexes[array[i]]++;
+        }
+
+        int max = 0;
+        int count = 0;
+        for (int j = 0; j < indexes.length; j++) {
+            if (count < indexes[j]) {
+                max = j;
+                count = indexes[j];
+            }
+        }
+        return max;
     }
 
 }
--- laboratore/Laborator2/src/Main.java
+++ laboratore/Laborator2/src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
-        int[] array1 = {1,2,2,3,4,4,4,4,6,6,6,6,6,6,7};
-        int[] array2 = {1,1,2,3,4,5,6,6,6,7,7,7,8,10,10};
+        int[] array1 = {1, 2, 2, 3, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 7};
+        int[] array2 = {1, 1, 2, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 10, 10};
 
         System.out.println("Per vektorin 1: ");
 
-        System.out.println("Indeksi i pare i 4: " + binarySearch.firstIndexSearch(array1, 1));
-        System.out.println("Indeksi i fundit i 4: " + binarySearch.lastIndexSearch(array1, 1));
+        for (int i = 1; i < 8; i++) {
+            System.out.println("Indeksi i pare i " + i + ": " + binarySearch.firstIndexSearch(array1, i));
+            System.out.println("Indeksi i fundit i " + i + ": " + binarySearch.lastIndexSearch(array1, i));
+            System.out.println();
+        }
+
+        System.out.println("Elementi me i perseritur " + binarySearch.findMostRepeatedValue(array1));
+
+        System.out.println("\n\n");
+
+        System.out.println("Per vektorin 2: ");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Indeksi i pare i " + i + ": " + binarySearch.firstIndexSearch(array2, i));
+            System.out.println("Indeksi i fundit i " + i + ": " + binarySearch.lastIndexSearch(array2, i));
+            System.out.println();
+        }
+
+        System.out.println("Elementi me i perseritur " + binarySearch.findMostRepeatedValue(array2));
+
+
     }
 }
